1e9b8a2cdd6b2d904628868143d434d9305f6c4c
V2Ray <admin@v2ray.com>
2015-09-05 23:03:56 +0200

    update vmess format

--- net/vmesshandler.go
+++ net/vmesshandler.go
@@ -1,9 +1,19 @@
 package net
 
+import (
+  "net"
+)
+
 type VMessHandler struct {
   
 }
 
 func (*VMessHandler) Listen(port uint8) error {
+  listener, err := net.Listen("tcp", ":" + string(port))
+  if err != nil {
+    return err
+  }
+  
+  
   return nil
 }
