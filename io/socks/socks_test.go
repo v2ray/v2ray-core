901434f0a18da8a520d994520b82bbb06b86cb0e
V2Ray <admin@v2ray.com>
2015-09-07 17:29:20 +0200

    format code

--- io/socks/socks_test.go
+++ io/socks/socks_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestAuthenticationResponseToBytes(t *testing.T) {
-  socksVersion := uint8(5)
-  authMethod := uint8(1)
+	socksVersion := uint8(5)
+	authMethod := uint8(1)
 	response := Socks5AuthenticationResponse{socksVersion, authMethod}
 	bytes := response.ToBytes()
 	if bytes[0] != socksVersion {
 		t.Errorf("Unexpected Socks version %d", bytes[0])
 	}
-  if bytes[1] != authMethod {
-    t.Errorf("Unexpected Socks auth method %d", bytes[1])
-  }
+	if bytes[1] != authMethod {
+		t.Errorf("Unexpected Socks auth method %d", bytes[1])
+	}
 }
