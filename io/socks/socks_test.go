07b440fdcb81a06f1a415dbab2460fdc4f077d84
V2Ray <admin@v2ray.com>
2015-09-07 17:28:36 +0200

    Test case for auth response tobytes

---
+++ io/socks/socks_test.go
@@ -0,0 +1,18 @@
+package socks
+
+import (
+	"testing"
+)
+
+func TestAuthenticationResponseToBytes(t *testing.T) {
+  socksVersion := uint8(5)
+  authMethod := uint8(1)
+	response := Socks5AuthenticationResponse{socksVersion, authMethod}
+	bytes := response.ToBytes()
+	if bytes[0] != socksVersion {
+		t.Errorf("Unexpected Socks version %d", bytes[0])
+	}
+  if bytes[1] != authMethod {
+    t.Errorf("Unexpected Socks auth method %d", bytes[1])
+  }
+}
