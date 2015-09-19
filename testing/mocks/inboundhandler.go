76200e27645b934cb835794abf9b2fb76057438e
V2Ray <admin@v2ray.com>
2015-09-19 21:56:09 +0200

    test case for net.vmess

---
+++ testing/mocks/inboundhandler.go
@@ -0,0 +1,38 @@
+package mocks
+
+import (
+	"bytes"
+
+	"github.com/v2ray/v2ray-core"
+	v2net "github.com/v2ray/v2ray-core/net"
+)
+
+type InboundConnectionHandler struct {
+	Data2Send    []byte
+	DataReturned *bytes.Buffer
+	Port         uint16
+	Server       *core.Point
+}
+
+func (handler *InboundConnectionHandler) Listen(port uint16) error {
+	handler.Port = port
+	return nil
+}
+
+func (handler *InboundConnectionHandler) Communicate(dest v2net.Address) error {
+	ray := handler.Server.NewInboundConnectionAccepted(dest)
+
+	input := ray.InboundInput()
+	output := ray.InboundOutput()
+
+	input <- handler.Data2Send
+	close(input)
+
+	v2net.ChanToWriter(handler.DataReturned, output)
+	return nil
+}
+
+func (handler *InboundConnectionHandler) Create(point *core.Point, config []byte) (core.InboundConnectionHandler, error) {
+	handler.Server = point
+	return handler, nil
+}
