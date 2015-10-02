0e1d71499377602ef2cb976ebcf5eabfa116d244
V2Ray <admin@v2ray.com>
2015-10-02 11:01:12 +0200

    update install.sh for x86

--- release/install.sh
+++ release/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+GO_AMD64=https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+GO_X86=https://storage.googleapis.com/golang/go1.5.1.linux-386.tar.gz
+ARCH=$(uname -m)
+GO_CUR=${GO_AMD64}
+
+if [ "$ARCH" == "i686" || "$ARCH" == "i386" ]; then
+  GO_CUR=${GO_X86}
+fi
+
 function git_not_installed {
   git --version 2>&1 >/dev/null
   GIT_IS_AVAILABLE=$?
@@ -12,7 +21,7 @@ fi
 
 
 if [ -z "$GOPATH" ]; then
-  curl -o go_latest.tar.gz https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+  curl -o go_latest.tar.gz ${GO_CUR}
   tar -C /usr/local -xzf go_latest.tar.gz
   rm go_latest.tar.gz
   export PATH=$PATH:/usr/local/go/bin
