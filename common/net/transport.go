cdea39ff9589ae7b342153ff3de309b8d2b22f7b
V2Ray <admin@v2ray.com>
2015-10-09 13:07:42 +0200

    Try writing again when there are some bytes left

--- common/net/transport.go
+++ common/net/transport.go
@@ -33,7 +33,10 @@ func ReaderToChan(stream chan<- *alloc.Buffer, reader io.Reader) error {
 // ChanToWriter dumps all content from a given chan to a writer until the chan is closed.
 func ChanToWriter(writer io.Writer, stream <-chan *alloc.Buffer) error {
 	for buffer := range stream {
-		_, err := writer.Write(buffer.Value)
+		nBytes, err := writer.Write(buffer.Value)
+    if nBytes < buffer.Len() {
+      _, err = writer.Write(buffer.Value[nBytes:])
+    }
 		buffer.Release()
 		if err != nil {
 			return err
