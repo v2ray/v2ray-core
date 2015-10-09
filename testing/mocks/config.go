b115df56c08aff26b80c5e1eb8134a594d84670b
V2Ray <admin@v2ray.com>
2015-10-09 17:58:35 +0200

    fix test break

--- testing/mocks/config.go
+++ testing/mocks/config.go
@@ -17,14 +17,27 @@ func (config *ConnectionConfig) Settings(config.Type) interface{} {
 	return config.SettingsValue
 }
 
+type LogConfig struct {
+	AccessLogValue string
+}
+
+func (config *LogConfig) AccessLog() string {
+	return config.AccessLogValue
+}
+
 type Config struct {
 	PortValue           uint16
+	LogConfigValue      *LogConfig
 	InboundConfigValue  *ConnectionConfig
 	OutboundConfigValue *ConnectionConfig
 }
 
 func (config *Config) Port() uint16 {
 	return config.PortValue
+}
+
+func (config *Config) LogConfig() config.LogConfig {
+	return config.LogConfigValue
 }
 
 func (config *Config) InboundConfig() config.ConnectionConfig {
