346b6125f4f2b732417da375c6980f773603fa29
V2Ray <admin@v2ray.com>
2015-11-04 22:48:54 +0100

    test cases for Destination

---
+++ common/net/destination_test.go
@@ -0,0 +1,25 @@
+package net
+
+import (
+	"testing"
+
+	"github.com/v2ray/v2ray-core/testing/unit"
+)
+
+func TestTCPDestination(t *testing.T) {
+	assert := unit.Assert(t)
+
+	dest := NewTCPDestination(IPAddress([]byte{1, 2, 3, 4}, 80))
+	assert.Bool(dest.IsTCP()).IsTrue()
+	assert.Bool(dest.IsUDP()).IsFalse()
+	assert.String(dest.String()).Equals("tcp:1.2.3.4:80")
+}
+
+func TestUDPDestination(t *testing.T) {
+	assert := unit.Assert(t)
+
+	dest := NewUDPDestination(IPAddress([]byte{0x20, 0x01, 0x48, 0x60, 0x48, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x88, 0x88}, 53))
+	assert.Bool(dest.IsTCP()).IsFalse()
+	assert.Bool(dest.IsUDP()).IsTrue()
+	assert.String(dest.String()).Equals("udp:[2001:4860:4860::8888]:53")
+}
