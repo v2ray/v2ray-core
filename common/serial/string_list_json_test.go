216faf587c3075c8fdaaab24c6ea64642e2c9c5d
v2ray <admin@v2ray.com>
2016-01-22 00:53:51 +0100

    string list test

---
+++ common/serial/string_list_json_test.go
@@ -0,0 +1,21 @@
+// +build json
+
+package serial_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	. "github.com/v2ray/v2ray-core/common/serial"
+	v2testing "github.com/v2ray/v2ray-core/testing"
+	"github.com/v2ray/v2ray-core/testing/assert"
+)
+
+func TestStringListUnmarshalError(t *testing.T) {
+	v2testing.Current(t)
+
+	rawJson := `1234`
+	list := new(StringLiteralList)
+	err := json.Unmarshal([]byte(rawJson), list)
+	assert.Error(err).IsNotNil()
+}
