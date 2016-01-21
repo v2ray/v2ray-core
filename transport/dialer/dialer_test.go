e346f179d28ba30a1c2379ce91c0baef7fb357b7
Darien Raymond <admin@v2ray.com>
2016-01-21 12:27:08 +0000

    update my own domain for testing

--- transport/dialer/dialer_test.go
+++ transport/dialer/dialer_test.go
@@ -4,16 +4,24 @@ import (
 	"testing"
 
 	v2net "github.com/v2ray/v2ray-core/common/net"
+	v2nettesting "github.com/v2ray/v2ray-core/common/net/testing"
 	v2testing "github.com/v2ray/v2ray-core/testing"
 	"github.com/v2ray/v2ray-core/testing/assert"
+	"github.com/v2ray/v2ray-core/testing/servers/tcp"
 	. "github.com/v2ray/v2ray-core/transport/dialer"
 )
 
 func TestDialDomain(t *testing.T) {
 	v2testing.Current(t)
 
-	conn, err := Dial(v2net.TCPDestination(v2net.DomainAddress("google.com"), 443))
+	server := &tcp.Server{
+		Port: v2nettesting.PickPort(),
+	}
+	dest, err := server.Start()
 	assert.Error(err).IsNil()
-	assert.StringLiteral(conn.RemoteAddr().Network()).Equals("tcp")
+
+	conn, err := Dial(v2net.TCPDestination(v2net.DomainAddress("local.v2ray.com"), dest.Port()))
+	assert.Error(err).IsNil()
+	assert.StringLiteral(conn.RemoteAddr().String()).Equals("127.0.0.1:" + dest.Port().String())
 	conn.Close()
 }
