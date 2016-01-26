6b894c719dabaf2ac9e2448b3940e3e0cb8c1933
Darien Raymond <admin@v2ray.com>
2016-01-26 10:28:09 +0000

    own hosts in http config

--- proxy/http/config.go
+++ proxy/http/config.go
@@ -1,4 +1,18 @@
 package http
 
+import (
+	v2net "github.com/v2ray/v2ray-core/common/net"
+)
+
 type Config struct {
+	OwnHosts []v2net.Address
+}
+
+func (this *Config) IsOwnHost(host v2net.Address) bool {
+	for _, ownHost := range this.OwnHosts {
+		if ownHost.Equals(host) {
+			return true
+		}
+	}
+	return false
 }
--- proxy/http/config_json.go
+++ proxy/http/config_json.go
@@ -3,12 +3,38 @@
 package http
 
 import (
+	"encoding/json"
+
+	v2net "github.com/v2ray/v2ray-core/common/net"
 	"github.com/v2ray/v2ray-core/proxy/internal/config"
 )
+
+func (this *Config) UnmarshalJSON(data []byte) error {
+	type JsonConfig struct {
+		Hosts []v2net.AddressJson `json:"ownHosts"`
+	}
+	jsonConfig := new(JsonConfig)
+	if err := json.Unmarshal(data, jsonConfig); err != nil {
+		return err
+	}
+	this.OwnHosts = make([]v2net.Address, len(jsonConfig.Hosts), len(jsonConfig.Hosts)+1)
+	for idx, host := range jsonConfig.Hosts {
+		this.OwnHosts[idx] = host.Address
+	}
+
+	v2rayHost := v2net.DomainAddress("local.v2ray.com")
+	if !this.IsOwnHost(v2rayHost) {
+		this.OwnHosts = append(this.OwnHosts, v2rayHost)
+	}
+
+	return nil
+}
 
 func init() {
 	config.RegisterInboundConfig("http",
 		func(data []byte) (interface{}, error) {
-			return new(Config), nil
+			rawConfig := new(Config)
+			err := json.Unmarshal(data, rawConfig)
+			return rawConfig, err
 		})
 }
