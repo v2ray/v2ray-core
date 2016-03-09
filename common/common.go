c7f6426c88d3b55b40fe1e234761098584217a83
v2ray <admin@v2ray.com>
2016-03-10 00:33:01 +0100

    release interface

--- common/common.go
+++ common/common.go
@@ -1,3 +1,15 @@
 // Package common contains common utilities that are shared among other packages.
 // See each sub-package for detail.
 package common
+
+import (
+	"errors"
+)
+
+var (
+	ErrorAlreadyReleased = errors.New("Object already released.")
+)
+
+type Releasable interface {
+	Release()
+}
