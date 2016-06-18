f54da94d03bd3175c2e78194fa3a61f3d9dc865f
v2ray <admin@v2ray.com>
2016-06-18 02:19:18 +0200

    update asm code

--- transport/internet/kcp/xor.go
+++ transport/internet/kcp/xor.go
@@ -1,7 +1,15 @@
 package kcp
 
 // xorfwd performs XOR forwards in words, x[i] ^= x[i-4], i from 0 to len
-func xorfwd(x []byte)
+func xorfwd(x []byte) {
+	for i := 4; i < len(x); i++ {
+		x[i] ^= x[i-4]
+	}
+}
 
 // xorbkd performs XOR backwords in words, x[i] ^= x[i-4], i from len to 0
-func xorbkd(x []byte)
+func xorbkd(x []byte) {
+	for i := len(x) - 1; i >= 4; i-- {
+		x[i] ^= x[i-4]
+	}
+}
