41225f8c12e8dfa6b92dedf6ad77bfd8d3861f21
V2Ray Dev <admin@v2ray.com>
2016-06-20 22:27:25 +0200

    correctly set tti

--- transport/internet/kcp/connection.go
+++ transport/internet/kcp/connection.go
@@ -80,7 +80,7 @@ func NewConnection(conv uint32, writerCloser io.WriteCloser, local *net.UDPAddr,
 	mtu := uint32(effectiveConfig.Mtu - block.HeaderSize() - headerSize)
 	conn.kcp = NewKCP(conv, mtu, conn.output)
 	conn.kcp.WndSize(effectiveConfig.GetSendingWindowSize(), effectiveConfig.GetReceivingWindowSize())
-	conn.kcp.NoDelay(1, 20, 2, effectiveConfig.Congestion)
+	conn.kcp.NoDelay(1, effectiveConfig.Tti, 2, effectiveConfig.Congestion)
 	conn.kcp.current = conn.Elapsed()
 
 	go conn.updateTask()
