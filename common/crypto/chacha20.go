b02bd5b1d84c79d9f65422a825fee2220ae7a230
v2ray <admin@v2ray.com>
2016-07-25 10:28:11 +0200

    rollback chacha20 change

--- common/crypto/chacha20.go
+++ common/crypto/chacha20.go
@@ -3,19 +3,9 @@ package crypto
 import (
 	"crypto/cipher"
 
-	"github.com/aead/chacha20"
+	"github.com/v2ray/v2ray-core/common/crypto/internal"
 )
 
-// NewChaCha20Stream creates a new Chacha/20 cipher stream. Caller must ensure that key is 32-bytes long and iv is either 8 or 12 bytes.
 func NewChaCha20Stream(key []byte, iv []byte) cipher.Stream {
-	var keyArray [32]byte
-	var nonce [12]byte
-	copy(keyArray[:], key)
-	switch len(iv) {
-	case 8:
-		copy(nonce[4:], iv)
-	case 12:
-		copy(nonce[:], iv)
-	}
-	return chacha20.NewCipher(&nonce, &keyArray)
+	return internal.NewChaCha20Stream(key, iv, 20)
 }
