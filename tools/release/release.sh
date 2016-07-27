db02a2dcc2a445db8aeaa6b74cc39acaaddcafb2
v2ray <admin@v2ray.com>
2016-07-27 17:50:24 +0200

    quick release script

---
+++ tools/release/release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+VER=$1
+MSG=$2
+
+if [ -z "$MSG" ]; then
+  MSG="Weekly Release"
+fi
+
+echo Creating a new release: $VER: $MSG
+
+IFS="." read -a PARTS <<< "$VER"
+MAJOR=${PARTS[0]}
+MINOR=${PARTS[1]}
+MINOR=$((MINOR+1))
+VERN=${MAJOR}.${MINOR}
+
+pushd $GOPATH/src/github.com/v2ray/v2ray-core
+echo "Adding a new tag: " "v$VER"
+git tag -s -a "v$VER" -m "$MSG"
+echo "Commiting core.go (may not necessary)"
+sed -i '' "s/\(version *= *\"\).*\(\"\)/\1$VERN\2/g" core.go
+echo "Pushing changes"
+git push --follow-tags
+popd
