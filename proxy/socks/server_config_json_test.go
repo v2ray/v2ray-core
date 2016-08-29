3423adaea4e39d1ad1aac696a0ea45af1a6a23e8
Darien Raymond <admin@v2ray.com>
2016-08-29 12:25:16 +0200

    fix test break

--- proxy/socks/server_config_json_test.go
+++ proxy/socks/server_config_json_test.go
@@ -17,5 +17,5 @@ func TestDefaultIPAddress(t *testing.T) {
     "auth": "noauth"
   }`))
 	assert.Error(err).IsNil()
-	assert.Address(socksConfig.(*ServerConfig).Address.AsAddress()).EqualsString("127.0.0.1")
+	assert.Address(socksConfig.(*ServerConfig).GetNetAddress()).EqualsString("127.0.0.1")
 }
