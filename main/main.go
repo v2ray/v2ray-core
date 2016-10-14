84f660bbbc12489ee58e71d2b999e851ae3eb113
Darien Raymond <admin@v2ray.com>
2016-10-14 23:41:17 +0200

    update main code

--- main/main.go
+++ main/main.go
@@ -11,25 +11,6 @@ import (
 
 	"v2ray.com/core"
 	"v2ray.com/core/common/log"
-
-	// The following are necessary as they register handlers in their init functions.
-	_ "v2ray.com/core/proxy/blackhole"
-	_ "v2ray.com/core/proxy/dokodemo"
-	_ "v2ray.com/core/proxy/freedom"
-	_ "v2ray.com/core/proxy/http"
-	_ "v2ray.com/core/proxy/shadowsocks"
-	_ "v2ray.com/core/proxy/socks"
-	_ "v2ray.com/core/proxy/vmess/inbound"
-	_ "v2ray.com/core/proxy/vmess/outbound"
-
-	_ "v2ray.com/core/transport/internet/kcp"
-	_ "v2ray.com/core/transport/internet/tcp"
-	_ "v2ray.com/core/transport/internet/udp"
-	_ "v2ray.com/core/transport/internet/ws"
-
-	_ "v2ray.com/core/transport/internet/authenticators/noop"
-	_ "v2ray.com/core/transport/internet/authenticators/srtp"
-	_ "v2ray.com/core/transport/internet/authenticators/utp"
 )
 
 var (
