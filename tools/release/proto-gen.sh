be026870f73628275d513d2730da43b3aedde97c
Darien Raymond <admin@v2ray.com>
2016-10-16 00:23:27 +0200

    protogen update

--- tools/release/proto-gen.sh
+++ tools/release/proto-gen.sh
@@ -5,7 +5,7 @@ for DIR in $(find ./v2ray.com/core -type d -not -path "*.git*"); do
   TEST_FILES=($DIR/*.proto)
   #echo ${TEST_FILES}
   if [ -f ${TEST_FILES[0]} ]; then
-    protoc --proto_path=. --proto_path=./github.com/google/protobuf/src --go_out=. $DIR/*.proto
+    protoc --proto_path=. --go_out=. $DIR/*.proto
   fi
 done
 popd
