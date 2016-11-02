1600a59254ba87c5046a4dad1f891883c32b6ad1
Darien Raymond <admin@v2ray.com>
2016-11-02 22:19:01 +0100

    enable tcp for shadowsocks server

--- proxy/shadowsocks/server.go
+++ proxy/shadowsocks/server.go
@@ -211,7 +211,7 @@ type ServerFactory struct{}
 
 func (this *ServerFactory) StreamCapability() v2net.NetworkList {
 	return v2net.NetworkList{
-		Network: []v2net.Network{v2net.Network_RawTCP},
+		Network: []v2net.Network{v2net.Network_TCP, v2net.Network_RawTCP},
 	}
 }
 
