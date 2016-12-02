ea27f7c434734a3e8c63d76ddba6c1ccb09ee4f2
Darien Raymond <admin@v2ray.com>
2016-12-02 13:12:45 +0100

    trigger official docker release on new tag

--- tools/release/release-ci.sh
+++ tools/release/release-ci.sh
@@ -14,3 +14,18 @@ $GOPATH/bin/build --os=freebsd --arch=x86 --zip
 $GOPATH/bin/build --os=freebsd --arch=amd64 --zip
 $GOPATH/bin/build --os=openbsd --arch=x86 --zip
 $GOPATH/bin/build --os=openbsd --arch=amd64 --zip
+
+INSTALL_DIR=_install
+
+git clone "https://github.com/v2ray/install.git" ${INSTALL_DIR}
+cp $GOPATH/bin/v2ray-${TRAVIS_TAG}-linux-64/v2ray ${INSTALL_DIR}/docker/official/
+
+pushd ${INSTALL_DIR}
+git config user.name "V2Ray Auto Build"
+git config user.email "admin@v2ray.com"
+git commit -m "Update for ${TRAVIS_TAG}"
+git push "https://${GIT_KEY_INSTALL}@github.com/v2ray/install.git" master
+popd
+
+DOCKER_HUB_API=https://registry.hub.docker.com/u/v2ray/official/trigger/${DOCKER_HUB_KEY}/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST "${DOCKER_HUB_API}"
