2f550186aca709ff9baab39306667a8205162db5
Darien Raymond <admin@v2ray.com>
2016-12-07 13:12:42 +0100

    comments

--- common/protocol/account.go
+++ common/protocol/account.go
@@ -1,11 +1,11 @@
 package protocol
 
+// Account is an user identity used for authentication.
 type Account interface {
 	Equals(Account) bool
 }
 
+// AsAccount is an object can be converted into account.
 type AsAccount interface {
 	AsAccount() (Account, error)
 }
-
-type NewAccountFactory func() AsAccount
