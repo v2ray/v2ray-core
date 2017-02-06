ce34a25e66f2b95739a53186a8b0b3c9efe232bf
Darien Raymond <admin@v2ray.com>
2017-02-06 13:49:03 +0100

    fix build break

--- transport/internet/kcp/crypt.go
+++ transport/internet/kcp/crypt.go
@@ -2,9 +2,9 @@ package kcp
 
 import (
 	"crypto/cipher"
+	"errors"
 	"hash/fnv"
 
-	"v2ray.com/core/common/crypto"
 	"v2ray.com/core/common/serial"
 )
 
@@ -64,12 +64,12 @@ func (v *SimpleAuthenticator) Open(dst, nonce, cipherText, extra []byte) ([]byte
 	fnvHash := fnv.New32a()
 	fnvHash.Write(dst[4:])
 	if serial.BytesToUint32(dst[:4]) != fnvHash.Sum32() {
-		return nil, crypto.ErrAuthenticationFailed
+		return nil, errors.New("KCP:SimpleAuthenticator: Invalid auth.")
 	}
 
 	length := serial.BytesToUint16(dst[4:6])
 	if len(dst)-6 != int(length) {
-		return nil, crypto.ErrAuthenticationFailed
+		return nil, errors.New("KCP:SimpleAuthenticator: Invalid auth.")
 	}
 
 	return dst[6:], nil
