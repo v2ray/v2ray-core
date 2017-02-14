9ca520893b75422f967aae7ad357cdbfa954cb84
Darien Raymond <admin@v2ray.com>
2017-02-14 09:26:17 +0100

    comments

--- transport/internet/websocket/ws.go
+++ transport/internet/websocket/ws.go
@@ -1,33 +1,5 @@
 /*Package websocket implements Websocket transport
 
 Websocket transport implements a HTTP(S) compliable, surveillance proof transport method with plausible deniability.
-
-To configure such a listener, set streamSettings to be ws. A http(s) listener will be listening at the port you have configured.
-
-There is additional configure can be made at transport configure.
-
-"wsSettings":{
-      "Path":"ws", // the path our ws handler bind
-      "Pto": "wss/ws", // the transport protocol we are using ws or wss(listen ws with tls)
-      "Cert":"cert.pem", // if you have configured to use wss, configure your cert here
-      "PrivKey":"priv.pem" //if you have configured to use wss, configure your privatekey here
-    }
-
-
-To configure such a Dialer, set streamSettings to be ws.
-
-There is additional configure can be made at transport configure.
-
-"wsSettings":{
-      "Path":"ws", // the path our ws handler bind
-      "Pto": "wss/ws", // the transport protocol we are using ws or wss(listen ws with tls)
-    }
-
-It is worth noting that accepting a non-valid cert is not supported as a self-signed or invalid cert can be a sign of a website that is not correctly configured and lead to additional investigation.
-
-
-This transport was disscussed at
-https://github.com/v2ray/v2ray-core/issues/224
-
 */
 package websocket
