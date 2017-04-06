954587cdc99506caa599bb98b61dd019f9f11185
Darien Raymond <admin@v2ray.com>
2017-04-06 23:11:26 +0200

    fix build for linux

--- transport/internet/tcp/sockopt_linux.go
+++ transport/internet/tcp/sockopt_linux.go
@@ -6,6 +6,7 @@ import (
 	"syscall"
 
 	"v2ray.com/core/app/log"
+	"v2ray.com/core/common/errors"
 	"v2ray.com/core/common/net"
 	"v2ray.com/core/transport/internet"
 )
@@ -15,18 +16,18 @@ const SO_ORIGINAL_DST = 80
 func GetOriginalDestination(conn internet.Connection) net.Destination {
 	tcpConn, ok := conn.(internet.SysFd)
 	if !ok {
-		log.Trace(errors.New("Dokodemo: Failed to get sys fd."))
+		log.Trace(errors.New("failed to get sys fd").Path("Transport", "Internet", "TCP"))
 		return net.Destination{}
 	}
 	fd, err := tcpConn.SysFd()
 	if err != nil {
-		log.Trace(errors.New("Dokodemo: Failed to get original destination: ", err))
+		log.Trace(errors.New("failed to get original destination").Base(err).Path("Transport", "Internet", "TCP"))
 		return net.Destination{}
 	}
 
 	addr, err := syscall.GetsockoptIPv6Mreq(fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST)
 	if err != nil {
-		log.Trace(errors.New("Dokodemo: Failed to call getsockopt: ", err))
+		log.Trace(errors.New("failed to call getsockopt").Base(err).Path("Transport", "Internet", "TCP"))
 		return net.Destination{}
 	}
 	ip := net.IPAddress(addr.Multiaddr[4:8])
