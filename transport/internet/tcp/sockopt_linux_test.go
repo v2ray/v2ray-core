3cb60fac903b1e38f3aeb89890e0045315f7485e
Darien Raymond <admin@v2ray.com>
2017-04-18 12:12:17 +0200

    fix test break

--- transport/internet/tcp/sockopt_linux_test.go
+++ transport/internet/tcp/sockopt_linux_test.go
@@ -8,20 +8,20 @@ import (
 
 	"v2ray.com/core/testing/assert"
 	"v2ray.com/core/testing/servers/tcp"
+	. "v2ray.com/core/transport/internet/tcp"
 )
 
 func TestGetOriginalDestination(t *testing.T) {
 	assert := assert.On(t)
 
-	tcpServer := tcp.Server{
-		MsgProcessor: xor,
-	}
+	tcpServer := tcp.Server{}
 	dest, err := tcpServer.Start()
 	assert.Error(err).IsNil()
 	defer tcpServer.Close()
 
 	conn, err := Dial(context.Background(), dest)
 	assert.Error(err).IsNil()
+	defer conn.Close()
 
 	_, err := GetOriginalDestination(conn)
 	assert.String(err.Error()).Contains("failed to call getsockopt")
