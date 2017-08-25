2b9d9e1ae391824962083c362be357e2df78042e
Darien Raymond <admin@v2ray.com>
2017-08-25 23:02:54 +0200

    remove use of sysFd

--- transport/internet/tcp/sockopt_linux.go
+++ transport/internet/tcp/sockopt_linux.go
@@ -3,27 +3,35 @@
 package tcp
 
 import (
-	"net"
 	"syscall"
 
 	v2net "v2ray.com/core/common/net"
 	"v2ray.com/core/transport/internet"
-	"v2ray.com/core/transport/internet/internal"
 )
 
 const SO_ORIGINAL_DST = 80
 
 func GetOriginalDestination(conn internet.Connection) (v2net.Destination, error) {
-	fd, err := internal.GetSysFd(conn.(net.Conn))
+	sysrawconn, f := conn.(syscall.Conn)
+	if !f {
+		return v2net.Destination{}, newError("unable to get syscall.Conn")
+	}
+	rawConn, err := sysrawconn.SyscallConn()
+	if err != nil {
+		return v2net.Destination{}, newError("failed to get sys fd").Base(err)
+	}
+	var dest v2net.Destination
+	err := rawConn.Control(func(fd uintptr) {
+		addr, err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
+		if err != nil {
+			return v2net.Destination{}, newError("failed to call getsockopt").Base(err)
+		}
+		ip := v2net.IPAddress(addr.Multiaddr[4:8])
+		port := uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
+		addr = v2net.TCPDestination(ip, v2net.Port(port))
+	})
 	if err != nil {
 		return v2net.Destination{}, newError("failed to get original destination").Base(err)
 	}
-
-	addr, err := syscall.GetsockoptIPv6Mreq(fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST)
-	if err != nil {
-		return v2net.Destination{}, newError("failed to call getsockopt").Base(err)
-	}
-	ip := v2net.IPAddress(addr.Multiaddr[4:8])
-	port := uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
-	return v2net.TCPDestination(ip, v2net.Port(port)), nil
+	return dest, nil
 }
