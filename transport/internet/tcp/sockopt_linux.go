5ef1a201f59a74e036f8134da08430b9b77e97b8
Darien Raymond <admin@v2ray.com>
2017-08-25 23:17:18 +0200

    fix test break

--- transport/internet/tcp/sockopt_linux.go
+++ transport/internet/tcp/sockopt_linux.go
@@ -5,6 +5,7 @@ package tcp
 import (
 	"syscall"
 
+	"v2ray.com/core/app/log"
 	v2net "v2ray.com/core/common/net"
 	"v2ray.com/core/transport/internet"
 )
@@ -21,14 +22,14 @@ func GetOriginalDestination(conn internet.Connection) (v2net.Destination, error)
 		return v2net.Destination{}, newError("failed to get sys fd").Base(err)
 	}
 	var dest v2net.Destination
-	err := rawConn.Control(func(fd uintptr) {
+	err = rawConn.Control(func(fd uintptr) {
 		addr, err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
 		if err != nil {
-			return v2net.Destination{}, newError("failed to call getsockopt").Base(err)
+			log.Trace(newError("failed to call getsockopt").Base(err))
 		}
 		ip := v2net.IPAddress(addr.Multiaddr[4:8])
 		port := uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
-		addr = v2net.TCPDestination(ip, v2net.Port(port))
+		dest = v2net.TCPDestination(ip, v2net.Port(port))
 	})
 	if err != nil {
 		return v2net.Destination{}, newError("failed to get original destination").Base(err)
