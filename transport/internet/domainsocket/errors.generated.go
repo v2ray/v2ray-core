5a7c49f3bdb173a58694059199acce210f26a4eb
Shelikhoo <xiaokangwang@outlook.com>
2017-11-03 12:39:43 +0800

    Added Generated error helper

---
+++ transport/internet/domainsocket/errors.generated.go
@@ -0,0 +1,5 @@
+package domainsocket
+
+import "v2ray.com/core/common/errors"
+
+func newError(values ...interface{}) *errors.Error { return errors.New(values...).Path("Transport", "Internet", "DomainSocket") }
