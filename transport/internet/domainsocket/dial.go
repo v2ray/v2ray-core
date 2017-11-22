b9dddd0586ff44989e9e692e45aa0110179e3033
Shelikhoo <xiaokangwang@outlook.com>
2017-11-22 21:53:22 +0800

    Added Dialer stub

--- transport/internet/domainsocket/dial.go
+++ transport/internet/domainsocket/dial.go
@@ -1,7 +1,19 @@
 package domainsocket
 
-import "context"
+import (
+	"context"
+	"net"
+)
 
-func DialDS(ctx context.Context, path string) {
+func DialDS(ctx context.Context, path string) (*net.UnixConn, error) {
+	resolvedAddress, err := net.ResolveUnixAddr("unixpacket", path)
+	if err != nil {
+		return nil, err
+	}
+	dialedUnix, err := net.DialUnix("unixpacket", nil, resolvedAddress)
+	if err != nil {
+		return nil, err
+	}
+	return dialedUnix, nil
 
 }
