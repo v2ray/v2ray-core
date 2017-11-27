973ce07db971f49a91dc3543b9ca2cd47daa24c0
Darien Raymond <admin@v2ray.com>
2017-11-27 23:25:43 +0100

    fix proto merging logic

--- app/policy/config.go
+++ app/policy/config.go
@@ -2,8 +2,6 @@ package policy
 
 import (
 	"time"
-
-	"github.com/golang/protobuf/proto"
 )
 
 func (s *Second) Duration() time.Duration {
@@ -11,5 +9,18 @@ func (s *Second) Duration() time.Duration {
 }
 
 func (p *Policy) OverrideWith(another *Policy) {
-	proto.Merge(p, another)
+	if another.Timeout != nil {
+		if another.Timeout.Handshake != nil {
+			p.Timeout.Handshake = another.Timeout.Handshake
+		}
+		if another.Timeout.ConnectionIdle != nil {
+			p.Timeout.ConnectionIdle = another.Timeout.ConnectionIdle
+		}
+		if another.Timeout.UplinkOnly != nil {
+			p.Timeout.UplinkOnly = another.Timeout.UplinkOnly
+		}
+		if another.Timeout.DownlinkOnly != nil {
+			p.Timeout.DownlinkOnly = another.Timeout.DownlinkOnly
+		}
+	}
 }
