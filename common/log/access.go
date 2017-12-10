4146590d528e3ed46d194f03b57098cdae10cdbf
Darien Raymond <admin@v2ray.com>
2017-12-10 22:48:28 +0100

    general purpose logger

---
+++ common/log/access.go
@@ -0,0 +1,15 @@
+package log
+
+type AccessStatus string
+
+const (
+	AccessAccepted = AccessStatus("accepted")
+	AccessRejected = AccessStatus("rejected")
+)
+
+type AccessMessage struct {
+	From   interface{}
+	To     interface{}
+	Status AccessStatus
+	Reason interface{}
+}
---
+++ common/log/log.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"sync/atomic"
+	"unsafe"
+
+	"v2ray.com/core/common/serial"
+)
+
+type Message interface {
+	String() string
+}
+
+type Handler interface {
+	Handle(msg Message)
+}
+
+type noOpHandler byte
+
+func (noOpHandler) Handle(msg Message) {}
+
+type GeneralMessage struct {
+	Severity Severity
+	Content  interface{}
+}
+
+func (m *GeneralMessage) String() string {
+	return serial.Concat("[", m.Severity, "]: ", m.Content)
+}
+
+func Record(msg Message) {
+	h := (*Handler)(atomic.LoadPointer(&logHandler))
+	(*h).Handle(msg)
+}
+
+var (
+	logHandler unsafe.Pointer
+)
+
+func RegisterHandler(handler Handler) {
+	if handler == nil {
+		panic("Log handler is nil")
+	}
+	atomic.StorePointer(&logHandler, unsafe.Pointer(&handler))
+}
+
+func init() {
+	RegisterHandler(noOpHandler(0))
+}
