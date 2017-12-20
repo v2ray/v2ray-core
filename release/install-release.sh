08019f4ac8dcf6d7e80f3f40c1a4518dd94af480
Ariselia <33754492+Ariselia@users.noreply.github.com>
2017-12-20 09:39:35 +0000

    Correction.

--- release/install-release.sh
+++ release/install-release.sh
@@ -240,7 +240,7 @@ installV2Ray(){
       let PORT=$RANDOM+10000
       UUID=$(cat /proc/sys/kernel/random/uuid)
       # Get public IP address
-      local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+      IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
       [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
       [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
 
