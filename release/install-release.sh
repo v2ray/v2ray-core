3d0c09718a0c67a3e2933438be5a8bd9f340d95a
Ariselia <33754492+Ariselia@users.noreply.github.com>
2017-12-20 09:20:53 +0000

    Show IP address when installation is done.

--- release/install-release.sh
+++ release/install-release.sh
@@ -245,9 +245,17 @@ installV2Ray(){
 
       colorEcho ${GREEN} "PORT:${PORT}"
       colorEcho ${GREEN} "UUID:${UUID}"
+      # Get public IP address
+      local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+      [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+      [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+      colorEcho ${GREEN} "IP:${IP}"
       mkdir -p /var/log/v2ray
     fi
     return 0
+    
+    
+
 }
 
 
