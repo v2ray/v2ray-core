3734195156631f861c4948941edc6a06f86f30a2
Darien Raymond <admin@v2ray.com>
2018-02-20 22:07:50 +0100

    fix log test

--- app/log/command/command_test.go
+++ app/log/command/command_test.go
@@ -5,8 +5,12 @@ import (
 	"testing"
 
 	"v2ray.com/core"
+	"v2ray.com/core/app/dispatcher"
 	"v2ray.com/core/app/log"
 	. "v2ray.com/core/app/log/command"
+	"v2ray.com/core/app/proxyman"
+	_ "v2ray.com/core/app/proxyman/inbound"
+	_ "v2ray.com/core/app/proxyman/outbound"
 	"v2ray.com/core/common/serial"
 	. "v2ray.com/ext/assert"
 )
@@ -17,6 +21,9 @@ func TestLoggerRestart(t *testing.T) {
 	v, err := core.New(&core.Config{
 		App: []*serial.TypedMessage{
 			serial.ToTypedMessage(&log.Config{}),
+			serial.ToTypedMessage(&dispatcher.Config{}),
+			serial.ToTypedMessage(&proxyman.InboundConfig{}),
+			serial.ToTypedMessage(&proxyman.OutboundConfig{}),
 		},
 	})
 
