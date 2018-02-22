cdfbd0317f0826bc09adbaff2d123312862865e9
Darien Raymond <admin@v2ray.com>
2018-02-22 11:58:46 +0100

    remove unused function

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -5,8 +5,6 @@
 // 2. Register a config creator through common.RegisterConfig.
 package proxy
 
-//go:generate go run $GOPATH/src/v2ray.com/core/common/errors/errorgen/main.go -pkg proxy -path Proxy
-
 import (
 	"context"
 
