f3f45c98bb5b3fef615f1dfd42ad679c841e84ce
MicahX <micahx@users.noreply.github.com>
2018-02-27 22:00:47 +0800

    TLS Forward Security Support

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -36,6 +36,14 @@ func (c *Config) GetTLSConfig() *tls.Config {
 
 	config.InsecureSkipVerify = c.AllowInsecure
 	config.Certificates = c.BuildCertificates()
+	config.CipherSuites := [...]uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	}
 	config.BuildNameToCertificate()
 	if len(c.ServerName) > 0 {
 		config.ServerName = c.ServerName
