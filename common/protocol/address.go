a52eb8f82b864fd78a75754e9ae90786adaf8e47
Darien Raymond <admin@v2ray.com>
2018-03-04 17:08:58 +0100

    allow underscore in domain name. fixes #917

--- common/protocol/address.go
+++ common/protocol/address.go
@@ -61,7 +61,7 @@ func maybeIPPrefix(b byte) bool {
 
 func isValidDomain(d string) bool {
 	for _, c := range d {
-		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '-' || c == '.') {
+		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '-' || c == '.' || c == '_') {
 			return false
 		}
 	}
