e1bdca446d1cf8092d7435cffed5672aa0c116aa
Darien Raymond <admin@v2ray.com>
2018-03-05 21:15:41 +0100

    fix context reference

--- app/log/command/command.go
+++ app/log/command/command.go
@@ -3,7 +3,8 @@ package command
 //go:generate go run $GOPATH/src/v2ray.com/core/common/errors/errorgen/main.go -pkg command -path App,Log,Command
 
 import (
-	context "golang.org/x/net/context"
+	"context"
+
 	grpc "google.golang.org/grpc"
 
 	"v2ray.com/core"
