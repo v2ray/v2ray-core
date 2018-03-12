0c213ccd209a6eea213ed0d98ca1483feaa80d98
Darien Raymond <admin@v2ray.com>
2018-03-12 16:24:31 +0100

    reset buffer when free

--- common/buf/buffer_pool.go
+++ common/buf/buffer_pool.go
@@ -47,6 +47,7 @@ func newBytes(size uint32) []byte {
 
 func freeBytes(b []byte) {
 	size := uint32(cap(b))
+	b = b[0:cap(b)]
 	for i := numPools - 1; i >= 0; i-- {
 		ps := poolSize[i]
 		if size >= ps {
