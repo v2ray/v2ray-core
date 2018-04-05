6d1faf5f6e33dcf4bae1403114ad4d64851620c1
Shelikhoo <xiaokangwang@outlook.com>
2018-04-05 19:28:30 +0800

    Remove staging code

--- app/proxyman/inbound/inbound.go
+++ app/proxyman/inbound/inbound.go
@@ -152,10 +152,6 @@ func NewHandler(ctx context.Context, config *core.InboundHandlerConfig) (core.In
 	return nil, newError("unknown allocation strategy: ", receiverSettings.AllocationStrategy.Type).AtError()
 }
 
-func (m *Manager) asUnixReceiverConfig(ctx context.Context, config *core.InboundHandlerConfig, unixrx proxyman.UnixReceiverConfig) {
-	return
-}
-
 func init() {
 	common.Must(common.RegisterConfig((*proxyman.InboundConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
 		return New(ctx, config.(*proxyman.InboundConfig))
