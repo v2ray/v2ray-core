320e1554bd14a5194c0986d446cd76100c9e9d1d
Darien Raymond <admin@v2ray.com>
2018-04-14 09:59:30 +0200

    update issue template

--- common/signal/semaphore.go
+++ common/signal/semaphore.go
@@ -1,9 +1,11 @@
 package signal
 
+// Semaphore is an implementation of semaphore.
 type Semaphore struct {
 	token chan struct{}
 }
 
+// NewSemaphore create a new Semaphore with n permits.
 func NewSemaphore(n int) *Semaphore {
 	s := &Semaphore{
 		token: make(chan struct{}, n),
@@ -14,10 +16,12 @@ func NewSemaphore(n int) *Semaphore {
 	return s
 }
 
+// Wait returns a channel for acquiring a permit.
 func (s *Semaphore) Wait() <-chan struct{} {
 	return s.token
 }
 
+// Signal releases a permit into the Semaphore.
 func (s *Semaphore) Signal() {
 	s.token <- struct{}{}
 }
