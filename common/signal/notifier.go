3b765a7b35d9fe1c4e46fb9e58604e3c916bf495
Darien Raymond <admin@v2ray.com>
2018-06-09 04:29:48 +0200

    remove unused code

--- common/signal/notifier.go
+++ common/signal/notifier.go
@@ -1,7 +1,5 @@
 package signal
 
-import "io"
-
 // Notifier is a utility for notifying changes. The change producer may notify changes multiple time, and the consumer may get notified asynchronously.
 type Notifier struct {
 	c chan struct{}
@@ -25,19 +23,4 @@ func (n *Notifier) Signal() {
 // Wait returns a channel for waiting for changes. The returned channel never gets closed.
 func (n *Notifier) Wait() <-chan struct{} {
 	return n.c
-}
-
-type nCloser struct {
-	n *Notifier
-}
-
-func (c *nCloser) Close() error {
-	c.n.Signal()
-	return nil
-}
-
-func NotifyClose(n *Notifier) io.Closer {
-	return &nCloser{
-		n: n,
-	}
 }
