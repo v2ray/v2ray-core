a31740870c0cd748305e9f489a2dece746181862
Milo <21700310+milohsu@users.noreply.github.com>
2018-06-25 13:09:38 +0800

    typo

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -36,7 +36,7 @@ func (c *Config) GetTLSConfig() *tls.Config {
 
 	config.InsecureSkipVerify = c.AllowInsecure
 	config.Certificates = c.BuildCertificates()
-	config.CipherSuites := [...]uint16{
+	config.CipherSuites := []uint16{
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
