c8a12bc35d230b9276d6e99e8d10e35ebfb182be
Darien Raymond <admin@v2ray.com>
2018-09-10 21:08:59 +0200

    move sockopt test back to its own file

--- transport/internet/sockopt_linux_test.go
+++ transport/internet/sockopt_linux_test.go
@@ -2,16 +2,18 @@ package internet_test
 
 import (
 	"context"
+	"syscall"
 	"testing"
 
 	"v2ray.com/core/common"
-	"v2ray.com/core/common/buf"
-	"v2ray.com/core/common/compare"
+	"v2ray.com/core/common/net"
 	"v2ray.com/core/testing/servers/tcp"
 	. "v2ray.com/core/transport/internet"
 )
 
-func TestTCPFastOpen(t *testing.T) {
+func TestSockOptMark(t *testing.T) {
+	t.Skip("requires CAP_NET_ADMIN")
+
 	tcpServer := tcp.Server{
 		MsgProcessor: func(b []byte) []byte {
 			return b
@@ -21,10 +23,11 @@ func TestTCPFastOpen(t *testing.T) {
 	common.Must(err)
 	defer tcpServer.Close()
 
+	const mark = 1
 	ctx := context.Background()
 	ctx = ContextWithStreamSettings(ctx, &MemoryStreamConfig{
 		SocketSettings: &SocketConfig{
-			Tfo: SocketConfig_Enable,
+			Mark: mark,
 		},
 	})
 	dialer := DefaultSystemDialer{}
@@ -32,12 +35,14 @@ func TestTCPFastOpen(t *testing.T) {
 	common.Must(err)
 	defer conn.Close()
 
-	_, err = conn.Write([]byte("abcd"))
+	rawConn, err := conn.(*net.TCPConn).SyscallConn()
 	common.Must(err)
-
-	b := buf.New()
-	common.Must(b.Reset(buf.ReadFrom(conn)))
-	if err := compare.BytesEqualWithDetail(b.Bytes(), []byte("abcd")); err != nil {
-		t.Fatal(err)
-	}
+	err = rawConn.Control(func(fd uintptr) {
+		m, err := syscall.GetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_MARK)
+		common.Must(err)
+		if mark != m {
+			t.Fatal("unexpected conneciton mark", m, " want ", mark)
+		}
+	})
+	common.Must(err)
 }
--- transport/internet/sockopt_unix.go
+++ transport/internet/sockopt_unix.go
@@ -8,21 +8,18 @@ import (
 )
 
 const (
-	// For incoming connections.
-	TCP_FASTOPEN = 23
-	// For out-going connections.
-	TCP_FASTOPEN_CONNECT = 30
+	TCP_FASTOPEN = 0x105
 )
 
 func applyOutboundSocketOptions(network string, address string, fd uintptr, config *SocketConfig) error {
 	if strings.HasPrefix(network, "tcp") {
 		switch config.Tfo {
 		case SocketConfig_Enable:
-			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_FASTOPEN_CONNECT, 1); err != nil {
+			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_FASTOPEN, 1); err != nil {
 				return err
 			}
 		case SocketConfig_Disable:
-			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_FASTOPEN_CONNECT, 0); err != nil {
+			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, TCP_FASTOPEN, 0); err != nil {
 				return err
 			}
 		}
