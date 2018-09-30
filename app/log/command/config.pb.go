56ce5d5f84e7f9efa6fd4ed9435079b0d8292c5d
Darien Raymond <admin@v2ray.com>
2018-09-30 23:08:14 +0200

    use new context

--- app/log/command/config.pb.go
+++ app/log/command/config.pb.go
@@ -1,11 +1,12 @@
 package command
 
 import (
+	"context"
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
 	math "math"
+
+	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
