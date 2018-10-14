e35e3e6e533362878036ac31015b832d0e13ef50
Darien Raymond <admin@v2ray.com>
2018-10-14 20:18:09 +0200

    comment

--- transport/internet/sockopt_darwin.go
+++ transport/internet/sockopt_darwin.go
@@ -7,8 +7,11 @@ import (
 )
 
 const (
-	TCP_FASTOPEN        = 0x105
+	// TCP_FASTOPEN is the socket option on darwin for TCP fast open.
+	TCP_FASTOPEN = 0x105
+	// TCP_FASTOPEN_SERVER is the value to enable TCP fast open on darwin for server connections.
 	TCP_FASTOPEN_SERVER = 0x01
+	// TCP_FASTOPEN_CLIENT is the value to enable TCP fast open on darwin for client connections.
 	TCP_FASTOPEN_CLIENT = 0x02
 )
 
