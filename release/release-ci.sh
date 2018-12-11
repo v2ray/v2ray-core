fd7bbdf5e9966c08a82375025951dd802e791a55
Darien Raymond <admin@v2ray.com>
2018-12-11 16:45:20 +0100

    update geosite on release

--- release/release-ci.sh
+++ release/release-ci.sh
@@ -62,6 +62,10 @@ popd
 GEOIP_TAG=$(curl --silent "https://api.github.com/repos/v2ray/geoip/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -L -o release/config/geoip.dat "https://github.com/v2ray/geoip/releases/download/${GEOIP_TAG}/geoip.dat"
 
+# Update geosite.dat
+GEOIP_TAG=$(curl --silent "https://api.github.com/repos/v2ray/domain-list-community/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -L -o release/config/geosite.dat "https://github.com/v2ray/domain-list-community/releases/download/${GEOIP_TAG}/dlc.dat"
+
 # Take a snapshot of all required source code
 pushd $GOPATH/src
 
