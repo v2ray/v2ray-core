57877d644e060a3ff19d72f892f0fca65e3094fc
Darien Raymond <admin@v2ray.com>
2019-01-17 15:58:42 +0100

    not necessary to flatten vendor directory now

--- release/release-ci.sh
+++ release/release-ci.sh
@@ -73,12 +73,6 @@ popd
 # Take a snapshot of all required source code
 pushd $GOPATH/src
 
-# Flatten vendor directories
-cp -r v2ray.com/core/vendor/github.com/ .
-rm -rf v2ray.com/core/vendor/
-cp -r github.com/lucas-clemente/quic-go/vendor/github.com/ .
-rm -rf github.com/lucas-clemente/quic-go/vendor/
-
 # Create zip file for all sources
 zip -9 -r /v2/build/src_all.zip * -x '*.git*'
 popd
