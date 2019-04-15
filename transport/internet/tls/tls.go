15c0e975ca66cc8ce3fb96e2943d04fb8b7a42de
liberal-boy <liberal-boy@protonmail.com>
2019-04-15 23:21:47 +0800

    enable TLS 1.3 by default

--- transport/internet/tls/tls.go
+++ transport/internet/tls/tls.go
@@ -4,6 +4,7 @@ package tls
 
 import (
 	"crypto/tls"
+	"os"
 
 	"v2ray.com/core/common/buf"
 	"v2ray.com/core/common/net"
@@ -64,4 +65,10 @@ func UClient(c net.Conn, config *tls.Config) net.Conn {
 func Server(c net.Conn, config *tls.Config) net.Conn {
 	tlsConn := tls.Server(c, config)
 	return &conn{Conn: tlsConn}
+}
+
+func init() {
+	// opt-in TLS 1.3 for Go1.12
+	// TODO: remove this line when Go1.13 is released.
+	_ = os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 }
