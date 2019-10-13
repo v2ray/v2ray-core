ed1ade4266693c311e4c97bdb0790e34fe16929a
nullptr <nullptr@nullptr.nullptr>
2019-10-13 12:12:49 +0800

    Add SO_REUSEPORT (kernel level load balance) support

--- transport/internet/sockopt_linux.go
+++ transport/internet/sockopt_linux.go
@@ -3,6 +3,7 @@ package internet
 import (
 	"net"
 	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -13,9 +14,12 @@ const (
 )
 
 func bindAddr(fd uintptr, ip []byte, port uint32) error {
-	err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
-	if err != nil {
+	if err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
 		return newError("failed to set resuse_addr").Base(err).AtWarning()
+	}
+
+	if err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+		return newError("failed to set resuse_port").Base(err).AtWarning()
 	}
 
 	var sockaddr syscall.Sockaddr
