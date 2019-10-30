b258ca729084cf6e77f9d2901e70e176fde534f0
Shelikhoo <xiaokangwang@outlook.com>
2019-10-30 18:46:33 +0800

    Set core version

--- core.go
+++ core.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	version  = "4.20.0"
+	version  = "4.20.5"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
