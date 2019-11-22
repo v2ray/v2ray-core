905ac82bb14e7e6b8ae77a3d5af42a7392648df9
vcptr <51714622+vcptr@users.noreply.github.com>
2019-11-22 16:26:59 +0800

    more test to common/buf

--- common/buf/reader_test.go
+++ common/buf/reader_test.go
@@ -1,10 +1,14 @@
 package buf_test
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"v2ray.com/core/common"
 	. "v2ray.com/core/common/buf"
 	"v2ray.com/core/transport/pipe"
@@ -65,11 +69,75 @@ func TestReadByte(t *testing.T) {
 	if b != 'a' {
 		t.Error("unexpected byte: ", b, " want a")
 	}
+	if reader.BufferedBytes() != 3 { // 3 bytes left in buffer
+		t.Error("unexpected buffered Bytes: ", reader.BufferedBytes())
+	}
 
 	nBytes, err := reader.WriteTo(DiscardBytes)
 	common.Must(err)
 	if nBytes != 3 {
 		t.Error("unexpect bytes written: ", nBytes)
+	}
+}
+
+func TestReadBuffer(t *testing.T) {
+	{
+		sr := strings.NewReader("abcd")
+		buf, err := ReadBuffer(sr)
+		common.Must(err)
+
+		if s := buf.String(); s != "abcd" {
+			t.Error("unexpected str: ", s, " want abcd")
+		}
+		buf.Release()
+	}
+
+	{
+		const dat = "data/test_ReadBuffer.dat"
+		f, err := os.Open(dat)
+		common.Must(err)
+		defer f.Close()
+
+		buf2, err := ReadBuffer(f)
+		common.Must(err)
+
+		cnt, err := ioutil.ReadFile(dat)
+		common.Must(err)
+
+		if cmp.Diff(buf2.Bytes(), cnt) != "" {
+			t.Error("fail to read from file")
+		}
+		buf2.Release()
+	}
+}
+
+func TestReadAtMost(t *testing.T) {
+	sr := strings.NewReader("abcd")
+	reader := &BufferedReader{
+		Reader: NewReader(sr),
+	}
+
+	mb, err := reader.ReadAtMost(3)
+	common.Must(err)
+	if s := mb.String(); s != "abc" {
+		t.Error("unexpected read result: ", s)
+	}
+
+	nBytes, err := reader.WriteTo(DiscardBytes)
+	common.Must(err)
+	if nBytes != 1 {
+		t.Error("unexpect bytes written: ", nBytes)
+	}
+}
+
+func TestPacketReader_ReadMultiBuffer(t *testing.T) {
+	const alpha = "abcefg"
+	buf := bytes.NewBufferString(alpha)
+	reader := &PacketReader{buf}
+	mb, err := reader.ReadMultiBuffer()
+	common.Must(err)
+	if s := mb.String(); s != alpha {
+		t.Error("content: ", s)
 	}
 }
 
