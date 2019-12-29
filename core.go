dc745d1e7a77aaac254cca06e0819bd8f7e915c4
kslr <kslrwang@gmail.com>
2019-12-29 16:55:01 +0800

    release 4.22.0

--- core.go
+++ core.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	version  = "4.20.5"
+	version  = "4.22.0"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
