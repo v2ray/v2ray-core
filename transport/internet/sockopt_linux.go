450bc5b3f08c28ac665a6b3f8490d52cb74c128b
nullptr <nullptr@nullptr.nullptr>
2020-02-08 17:54:28 +0800

    Add SO_REUSEPORT to inbound

--- transport/internet/sockopt_linux.go
+++ transport/internet/sockopt_linux.go
@@ -104,5 +104,9 @@ func applyInboundSocketOptions(network string, fd uintptr, config *SocketConfig)
 		}
 	}
 
+	if err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+		return newError("failed to set SO_REUSEPORT").Base(err).AtWarning()
+	}
+
 	return nil
 }
