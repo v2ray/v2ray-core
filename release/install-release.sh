4a3d2ad0b4cbdc058cba52e448fb4549bec48034
mzz <m@mzz.pub>
2020-02-15 18:48:43 +0800

    fix: download from jsdelivr using specific version now
    download from jsdelivr using specific version now to avoid not updated cdn cache.

--- release/install-release.sh
+++ release/install-release.sh
@@ -144,7 +144,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     if [[ "${DIST_SRC}" == "jsdelivr" ]]; then
-        DOWNLOAD_LINK="https://cdn.jsdelivr.net/gh/v2ray/dist/v2ray-linux-${VDIS}.zip"
+        DOWNLOAD_LINK="https://cdn.jsdelivr.net/gh/v2ray/dist@${NEW_VER}/v2ray-linux-${VDIS}.zip"
     else
         DOWNLOAD_LINK="https://github.com/v2ray/v2ray-core/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
     fi
