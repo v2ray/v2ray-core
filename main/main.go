37a55653e9e972f43854612cc980a6e1ea812544
Agioi Theodoroi <agioi@protonmail.com>
2020-04-17 20:00:40 -0400

    remove unused parameter

--- main/main.go
+++ main/main.go
@@ -166,7 +166,7 @@ func main() {
 
 	{
 		osSignals := make(chan os.Signal, 1)
-		signal.Notify(osSignals, os.Interrupt, os.Kill, syscall.SIGTERM)
+		signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
 		<-osSignals
 	}
 }
