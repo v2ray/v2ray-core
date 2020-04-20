a3e999db73656a8c70b87cb0fef8e3c98e415ff9
ZhiFeng Hu <root@yskm.dev>
2020-04-20 15:07:39 +0800

    Revert "优化重试策略"
    This reverts commit d1b54d450fcbbd37f525e02eba239d299a2ea8b7.

--- app/proxyman/inbound/worker.go
+++ app/proxyman/inbound/worker.go
@@ -95,10 +95,7 @@ func (w *tcpWorker) callback(conn internet.Connection) {
 		}
 	}
 	if err := w.proxy.Process(ctx, net.Network_TCP, conn, w.dispatcher); err != nil {
-		//try again
-		if err := w.proxy.Process(ctx, net.Network_TCP, conn, w.dispatcher); err != nil {
-			newError("connection ends").Base(err).WriteToLog(session.ExportIDToError(ctx))
-		}
+		newError("connection ends").Base(err).WriteToLog(session.ExportIDToError(ctx))
 	}
 	cancel()
 	if err := conn.Close(); err != nil {
