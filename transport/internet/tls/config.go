e62e6608e1d8ad480de74e0d533601bc7bc4e075
vcptr <51714622+vcptr@users.noreply.github.com>
2020-05-31 02:19:34 +0800

    tls use crypto std cipher suites

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -187,23 +187,10 @@ func (c *Config) GetTLSConfig(opts ...Option) *tls.Config {
 	}
 
 	if !c.AllowInsecureCiphers && len(config.CipherSuites) == 0 {
-		config.CipherSuites = []uint16{
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		// use tls cipher suites from cryto/tls
+		config.CipherSuites = []uint16{}
+		for _, s := range tls.CipherSuites() {
+			config.CipherSuites = append(config.CipherSuites, s.ID)
 		}
 	}
 
