2c8f06e121f276b704a44739452570b2e50f5a7f
Kslr <kslrwang@gmail.com>
2020-06-19 21:57:03 +0800

    clean code (#22)

--- release/updatedat.sh
+++ release/updatedat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+pushd "$GOPATH/src/v2ray.com/core/" || return
 
-pushd $GOPATH/src/v2ray.com/core/
 # Update geoip.dat
 GEOIP_TAG=$(curl --silent "https://api.github.com/repos/v2ray/geoip/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -L -o release/config/geoip.dat "https://github.com/v2ray/geoip/releases/download/${GEOIP_TAG}/geoip.dat"
@@ -11,4 +11,5 @@ sleep 1
 GEOSITE_TAG=$(curl --silent "https://api.github.com/repos/v2ray/domain-list-community/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -L -o release/config/geosite.dat "https://github.com/v2ray/domain-list-community/releases/download/${GEOSITE_TAG}/dlc.dat"
 sleep 1
-popd
+
+popd || return
