9142c0d9a8412be04405713d7049d0257ae0c611
Shelikhoo <xiaokangwang@outlook.com>
2020-07-02 20:27:34 +0800

    Generate manifest on tag release

--- release/bleedingrelease.sh
+++ release/bleedingrelease.sh
@@ -6,7 +6,7 @@ JSON_DATA=$(echo ${JSON_DATA} | jq -c ".prerelease=${PRERELEASE}")
 JSON_DATA=$(echo ${JSON_DATA} | jq -c ".body=\"${RELBODY}\"")
 RELEASE_DATA=$(curl --data "${JSON_DATA}" -H "Authorization: token ${GITHUB_TOKEN}" -X POST https://api.github.com/repos/v2fly/V2FlyBleedingEdgeBinary/releases)
 echo $RELEASE_DATA
-RELEASE_ID=$(echo $RELEASE_DATA| jq ".id")
+RELEASE_ID=$(echo $RELEASE_DATA | jq ".id")
 
 function uploadfile() {
   FILE=$1
@@ -20,10 +20,10 @@ function uploadfile() {
 function upload() {
   FILE=$1
   DGST=$1.dgst
-  openssl dgst -md5 $FILE | sed 's/([^)]*)//g' >> $DGST
-  openssl dgst -sha1 $FILE | sed 's/([^)]*)//g' >> $DGST
-  openssl dgst -sha256 $FILE | sed 's/([^)]*)//g' >> $DGST
-  openssl dgst -sha512 $FILE | sed 's/([^)]*)//g' >> $DGST
+  openssl dgst -md5 $FILE | sed 's/([^)]*)//g' >>$DGST
+  openssl dgst -sha1 $FILE | sed 's/([^)]*)//g' >>$DGST
+  openssl dgst -sha256 $FILE | sed 's/([^)]*)//g' >>$DGST
+  openssl dgst -sha512 $FILE | sed 's/([^)]*)//g' >>$DGST
   uploadfile $FILE
   uploadfile $DGST
 }
