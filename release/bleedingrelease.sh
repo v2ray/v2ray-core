dadcda034dc38a5d924d5a917fa8baba2205b5a5
Shelikhoo <xiaokangwang@outlook.com>
2020-07-02 19:10:38 +0800

    Generate Manifest for bleeding edge release

--- release/bleedingrelease.sh
+++ release/bleedingrelease.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 RELBODY="https://www.v2ray.com/chapter_00/01_versions.html"
 JSON_DATA=$(echo "{}" | jq -c ".tag_name=\"${RELEASE_TAG}\"")
 JSON_DATA=$(echo ${JSON_DATA} | jq -c ".prerelease=${PRERELEASE}")
@@ -31,6 +30,38 @@ function upload() {
 
 ART_ROOT=$GOPATH/src/v2ray.com/core/bazel-bin/release
 
+pushd ART_ROOT
+{
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen version ${RELEASE_TAG}
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen project "v2flyunstable"
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-macos.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-windows-64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-windows-32.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-windows-arm.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-arm64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-mips64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-mips64le.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-mips.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-mipsle.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-ppc64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-ppc64le.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-linux-s390x.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-freebsd-64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-freebsd-32.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-openbsd-64.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-openbsd-32.zip
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen file v2ray-dragonfly-64.zip
+} >Release.unsigned.unsorted
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil gen sort < Release.unsigned.unsorted > Release.unsigned
+
+  {
+    echo "Build Finished"
+    echo "https://github.com/v2fly/V2FlyBleedingEdgeBinary/releases/tag/${RELEASE_TAG}"
+  } > buildcomment
+
+  go run github.com/xiaokangwang/V2BuildAssist/v2buildutil post commit "${RELEASE_SHA}" < buildcomment
+popd
+
 upload ${ART_ROOT}/v2ray-macos.zip
 upload ${ART_ROOT}/v2ray-windows-64.zip
 upload ${ART_ROOT}/v2ray-windows-32.zip
@@ -51,3 +82,4 @@ upload ${ART_ROOT}/v2ray-freebsd-32.zip
 upload ${ART_ROOT}/v2ray-openbsd-64.zip
 upload ${ART_ROOT}/v2ray-openbsd-32.zip
 upload ${ART_ROOT}/v2ray-dragonfly-64.zip
+upload ${ART_ROOT}/Release.unsigned
