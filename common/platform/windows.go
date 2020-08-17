bc008113b6d1654e34ed372a385cc9dd0a0e2936
vcptr <51714622+vcptr@users.noreply.github.com>
2020-08-17 18:56:01 +0800

    add asset location search

--- common/platform/windows.go
+++ common/platform/windows.go
@@ -18,3 +18,10 @@ func GetToolLocation(file string) string {
 	toolPath := EnvFlag{Name: name, AltName: NormalizeEnvName(name)}.GetValue(getExecutableDir)
 	return filepath.Join(toolPath, file+".exe")
 }
+
+// GetAssetLocation search for `file` in the excutable dir
+func GetAssetLocation(file string) string {
+	const name = "v2ray.location.asset"
+	assetPath := NewEnvFlag(name).GetValue(getExecutableDir)
+	return filepath.Join(assetPath, file)
+}
