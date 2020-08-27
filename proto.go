cafc80d23ad82ac11b40658e0e4c72f789da5bfc
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2020-08-27 16:37:59 +0800

    vprotogen: do NOT rely on GOBIN & GOPATH

--- proto.go
+++ proto.go
@@ -1,8 +1,7 @@
 package core
 
-//go:generate go install "google.golang.org/protobuf/proto"
-//go:generate go install "google.golang.org/protobuf/cmd/protoc-gen-go"
-//go:generate go install "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-//go:generate go install "github.com/gogo/protobuf/protoc-gen-gofast"
-//go:generate go install "v2ray.com/core/infra/vprotogen"
-//go:generate vprotogen -repo v2ray.com/core
+//go:generate go install google.golang.org/protobuf/proto
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go get -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@v0.0.0-20200825170228-39ef2aaf62df
+//go:generate go install github.com/gogo/protobuf/protoc-gen-gofast
+//go:generate go run v2ray.com/core/infra/vprotogen
