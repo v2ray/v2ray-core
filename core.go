ad4924ac4e64caaa7b7e92051ed3231015a8ac4a
Kslr <kslrwang@gmail.com>
2020-09-25 17:38:14 +0800

    bump version

--- core.go
+++ core.go
@@ -19,7 +19,7 @@ import (
 )
 
 var (
-	version  = "4.28.2"
+	version  = "4.29.0"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
