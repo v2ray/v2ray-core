edefca79321ba72d8a99e4e7ac6d4c09e05d8274
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2020-10-04 09:56:45 +0800

    Refine & make proto.go work outside GOPATH

--- proto.go
+++ proto.go
@@ -1,7 +1,6 @@
 package core
 
-//go:generate go install google.golang.org/protobuf/proto
-//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
-//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate go install github.com/gogo/protobuf/protoc-gen-gofast
-//go:generate go run v2ray.com/core/infra/vprotogen
+//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast
+//go:generate go run ./infra/vprotogen/main.go
