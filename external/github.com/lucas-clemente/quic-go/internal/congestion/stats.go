a44e1ede5b385b9e793813fed7277d0918878c90
Kslr <kslrwang@gmail.com>
2020-10-09 10:04:35 +0800

    update to IETF QUIC draft-29 (#287)
    * update to IETF QUIC draft-29

--- external/github.com/lucas-clemente/quic-go/internal/congestion/stats.go
+++
@@ -1,8 +0,0 @@
-package congestion
-
-import "v2ray.com/core/external/github.com/lucas-clemente/quic-go/internal/protocol"
-
-type connectionStats struct {
-	slowstartPacketsLost protocol.PacketNumber
-	slowstartBytesLost   protocol.ByteCount
-}
